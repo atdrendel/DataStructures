2da2db089bf2ccfb461026cec0c3d34a6b52133b
Anthony Drendel <anthony.drendel@microsoft.com>
2017-01-01 17:37:18 -0600

    Add forEach() tests for Stack

--- Tests/DataStructuresTests/StackTests.swift
+++ Tests/DataStructuresTests/StackTests.swift
@@ -67,6 +67,53 @@ class StackTests: XCTestCase {
         XCTAssertNil(stack.peek())
     }
 
+    func testForEach() {
+        class Object {
+            var value: Int = 0
+        }
+
+        let one = Object()
+        let two = Object()
+        let three = Object()
+
+        var stack = Stack<Object>()
+        stack.push(one)
+        stack.push(two)
+        stack.push(three)
+
+        stack.forEach { $0.value += 1 }
+        XCTAssertEqual(1, one.value)
+        XCTAssertEqual(1, two.value)
+        XCTAssertEqual(1, three.value)
+    }
+
+    func testForEachThrows() {
+        struct ObjectError: Error { }
+
+        class Object {
+            var value: Int = 0
+
+            func increment(throw: Bool) throws {
+                if `throw` { throw ObjectError() }
+                else { value += 1 }
+            }
+        }
+
+        let one = Object()
+        let two = Object()
+
+        var stack = Stack<Object>()
+        stack.push(one)
+        stack.push(two)
+
+        XCTAssertThrowsError(try stack.forEach({ try $0.increment(throw: true) })) { error in
+            XCTAssertTrue(error is ObjectError)
+        }
+
+        XCTAssertEqual(0, one.value)
+        XCTAssertEqual(0, two.value)
+    }
+
     func testDescription() {
         var stack = Stack<String>()
         stack.push("1")
